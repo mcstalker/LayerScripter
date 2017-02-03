65fb50602a0e4cbaf79ce4e504b149d6b3d9a050
mcstalker <mcstalker@comcast.net>
2017-02-03 00:10:12 -0600

    V0.6b Adds:  - Added Debug function. Changes:  - Changed all functions to named functions. Fixes:

---
+++ Script1.js
@@ -0,0 +1,24 @@
+function Alerter(str) {
+    var myName = arguments.callee.toString();
+    log('Alerter::' + str + '::' + myName + '::');
+    log('Alerter::' + str + '::' + arguments.callee.name + '::');
+};
+
+var Alerter2 = function (str) {
+    var myName = arguments.callee.toString();
+    log('Alerter2::' + str + '::' + myName + '::');
+    log('Alerter2::' + str + '::' + arguments.callee.name + '::');
+    log('Alerter2::' + str + '::' + Alerter2.name + '::');
+};
+
+log('Blank Space::' + str + '::' + Alerter2.name + '::');
+
+on('ready', function () {
+    'use strict';
+
+    var str = 'test message';
+    Alerter(str);
+    Alerter2(str);
+    log('on(ready)::' + str + '::' + Alerter2.name + '::');
+    sendChat('me', str);
+});
